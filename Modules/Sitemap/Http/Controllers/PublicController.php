54d17eb7766891c9b1046fbfc6d477e0d61b7ad1
anph1991 <phamhoaian005@gmail.com>
2018-02-07 11:55:29 +0700

    public sitemap module

---
+++ Modules/Sitemap/Http/Controllers/PublicController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace TypiCMS\Modules\Sitemap\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+use TypiCMS\Modules\Core\Facades\TypiCMS;
+use TypiCMS\Modules\Pages\Facades\Facade as Pages;
+
+class PublicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Support\Facades\Response
+     */
+    public function generate()
+    {
+        // create new sitemap object
+        $sitemap = app('sitemap');
+
+        // set cache (key (string), duration in minutes (Carbon|Datetime|int), turn on/off (boolean))
+        // by default cache is disabled
+        if (config('typicms.cache')) {
+            $sitemap->setCache('laravel.sitemap', 3600);
+        }
+
+        // check if there is cached sitemap and build new only if is not
+        if (!$sitemap->isCached()) {
+            foreach (TypiCMS::getOnlineLocales() as $locale) {
+                app()->setLocale($locale);
+
+                $pages = Pages::allBy('private', 0);
+
+                foreach ($pages as $page) {
+                    $url = url($page->uri($locale));
+                    $sitemap->add($url, $page->updated_at);
+
+                    if (!$module = ucfirst($page->module)) {
+                        continue;
+                    }
+
+                    if (!class_exists($module)) {
+                        continue;
+                    }
+
+                    foreach ($module::all() as $item) {
+                        $url = url($item->uri($locale));
+                        $sitemap->add($url, $item->updated_at);
+                    }
+                }
+            }
+        }
+
+        // show your sitemap (options: 'xml' (default), 'html', 'txt', 'ror-rss', 'ror-rdf')
+        return $sitemap->render('xml');
+    }
+}
---
+++ Modules/Sitemap/Providers/RouteServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace TypiCMS\Modules\Sitemap\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Routing\Router;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * This namespace is applied to the controller routes in your routes file.
+     *
+     * In addition, it is set as the URL generator's root namespace.
+     *
+     * @var string
+     */
+    protected $namespace = 'TypiCMS\Modules\Sitemap\Http\Controllers';
+
+    /**
+     * Define the routes for the application.
+     *
+     * @param \Illuminate\Routing\Router $router
+     *
+     * @return void
+     */
+    public function map(Router $router)
+    {
+        $router->group(['namespace' => $this->namespace], function (Router $router) {
+            /*
+             * Front office routes
+             */
+            $router->get('sitemap.xml', 'PublicController@generate')->name('sitemap');
+        });
+    }
+}
